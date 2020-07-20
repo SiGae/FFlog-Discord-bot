16ab30385d458449a836ddd506517362fdddee4e
SiGae <sigae@protonmail.com>
2020-07-20 15:29:02 +0900

    구조개선, 절알렉산더 대응, 클래스별 퍼센트 추가, 커맨드 변경

--- bot.py
+++ bot.py
@@ -5,8 +5,123 @@ import json
 import urllib.request
 from urllib import parse
 import requests
+from operator import itemgetter
 
 client = discord.Client()
+
+
+def SliceByLayer(rawData):
+    output = []
+    classflag = []
+    layer = []
+    cnt = -1
+    for i in rawData:
+        if i['difficulty'] == 101 or i['difficulty'] == 100:
+            layer.append({
+                'encounterID' : i['encounterID'],
+                'spec' : i['spec'],
+                'percentile' : i['percentile']
+            }
+        )
+
+    for i in layer:
+        if not(i['encounterID'] in classflag):
+            output.append([])
+            classflag.append(i['encounterID'])
+            cnt += 1
+        output[cnt].append(i)
+    print(classflag)
+
+    return output
+
+def sortedByPercentile(rawData):
+    output = []
+    for i in rawData:
+        data = sorted(i, key = itemgetter('percentile', 'spec'), reverse = True)
+        output.append(data)
+    return output
+
+def SliceBySpecDetail(rawData):
+    spec = []
+    output = []
+    data = sorted(rawData, key=itemgetter('spec', 'percentile'), reverse = True)
+    for i in data:
+        if not(i['spec'] in spec):
+            spec.append(i['spec'])
+            output.append(i)
+    return output
+
+def SliceBySpec(rawData):
+    output = []
+    for i in rawData:
+        output.append(SliceBySpecDetail(i))
+    output = sortedByPercentile(output)
+    return output
+
+def Alexander(rawData, job, server):
+    msg = rawData.split()
+
+    out = []
+    cl = []
+    
+    lc ="https://www.fflogs.com:443/v1/parses/character/{}/{}/KR".format(msg[2], server[msg[1]])
+    param = {
+        'api_key' : private.ffkey,
+        'metric' : "rdps",
+        'zone' : 32,
+        'timeframe' : 'historical'
+
+    }
+
+    urlopen = requests.get(lc, params = param)
+    js = json.loads(urlopen.text)
+
+    print(js)
+
+    
+    data = SliceBySpec(SliceByLayer(js))
+    cnt = "절알렉산더"
+    workString = ""
+    for i in data:
+        workString += "{}\n".format(cnt)
+        workSubString = ""
+        for j in i:
+            workSubString += "\t{}: {}\n".format(job[j['spec']], j['percentile'])
+        workString += workSubString
+        workString += "\n"
+    return workString
+
+def edenAwake(rawData, job, server):
+    msg = rawData.split()
+
+    out = []
+    cl = []
+    
+    lc ="https://www.fflogs.com:443/v1/parses/character/{}/{}/KR".format(msg[2], server[msg[1]])
+    param = {
+        'api_key' : private.ffkey,
+        'metric' : "rdps",
+        'zone' : 29,
+        'timeframe' : 'historical'
+
+    }
+    urlopen = requests.get(lc, params = param)
+    js = json.loads(urlopen.text)
+
+    
+    data = SliceBySpec(SliceByLayer(js))
+    cnt = 1
+    workString = ""
+    for i in data:
+        workString += "{}.\n".format(cnt)
+        workSubString = ""
+        cnt +=1
+        for j in i:
+            workSubString += "\t{}: {}\n".format(job[j['spec']], j['percentile'])
+        workString += workSubString
+        workString += "\n"
+    return workString
+
 
 @client.event
 async def on_message(message):
@@ -39,36 +154,36 @@ async def on_message(message):
     '톤베리' : 'tonberry'
     }
     print(message.content)
-    if message.content.startswith('/ff'):
+
+    if message.content.startswith('/ffall'):
         msg = message.content.split()
+        workString = Alexander(message.content, job, server)
+        print(workString) 
 
-        out = []
-        cl = []
-        
-        lc ="https://www.fflogs.com:443/v1/parses/character/{}/{}/KR".format(msg[2], server[msg[1]])
-        param = {
-            'api_key' : private.ffkey,
-            'metric' : "rdps",
-            'zone' : 29,
-            'timeframe' : 'historical'
 
-        }
-        urlopen = requests.get(lc, params = param)
-        js = json.loads(urlopen.text)
+        await message.channel.send('```\n{}\n```'.format(workString))
 
-        cnt = 65
-        for i in js:
+        workString = edenAwake(message.content, job, server)
+        print(workString) 
 
-            if cnt == i['encounterID'] and 101 == i['difficulty']:
-                out.append(i['percentile'])
-                cl.append(job[i['spec']])
-                cnt += 1
-        
-        while len(out) != 4:
-            out.append("None")
-            cl.append("None")
 
-        await message.channel.send('```[1. {}]\n{}\n[2. {}]\n{}\n[3. {}]\n{}\n[4. {}]\n{}```'.format(cl[0], out[0],cl[1], out[1],cl[2], out[2],cl[3], out[3]))
+        await message.channel.send('```\n각영\n{}\n```'.format(workString))
+
+    if message.content.startswith('/ffua'):
+        msg = message.content.split()
+        workString = Alexander(message.content, job, server)
+        print(workString) 
+
+
+        await message.channel.send('```\n{}\n```'.format(workString))
+
+    if message.content.startswith('/ffeg'):
+
+        workString = edenAwake(message.content, job, server)
+        print(workString) 
+
+
+        await message.channel.send('```\n{}\n```'.format(workString))
 
 client.run(private.key)
 
